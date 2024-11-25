3a2772688851d001a958fa51dd87013bd480e07f
Michael Pymer <mikepymer@hotmail.co.uk>
2024-11-25 15:47:15 +0000

    changed check answer function partially. reseting to get rid of copilot

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer() ;
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -38,10 +38,25 @@ function runGame(gameType) {
     }
 }
 
-runGame();
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
 
 function checkAnswer() {
 
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwwww.... you answered ${userAnswer})
+    }
+ 
+
+    
 }
 
 /** 
